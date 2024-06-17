f71b2c90004001dc29b7643b3562282f70e3363f
amaxharraj <amaxharraj@stud.hs-heilbronn.de>
2024-06-17 16:53:30 +0200

    Aufgabe 1

--- Aufgaben/aufgabe1.py
+++ Aufgaben/aufgabe1.py
@@ -1,16 +1,19 @@
 #AUFGABE 1
 
-#
+# a) Erstellung einer Konfigurationsdatei und "UPPER_CASE"-Warnung entfernen bzw. unterdrücken.
+
+#    'pylint --generate-rcfile > workshop.pylintrc'
+#    'TIPP: Cheat-Sheet (Messages Controll)'
 
 
-#Verbessere den Code aus der Einführung
+# b) Verbessere den Code aus der Einführung (Calculations_2.py)
 
-def calc(r):
-    p=3.14159
-    a=p*r**2
-    return a
+#def calc(r):
+#   p=3.14159
+#   a=p*r**
+#   return a
 
 
-rad=5
-area=calc(rad)
-print("Area", area) 
+#rad=5
+#area=calc(rad)
+#print("Area", area)"
